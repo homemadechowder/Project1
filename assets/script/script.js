b802c393460ab7a3c0f34a25e41fbff309ce7562
Andrew Lu <awclu@ucdavis.edu>
2019-11-06 21:27:15 -0800

    added input method

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,16 +1,29 @@
 //EDAMAM Api Key = d2d2590ec9988c392da648e07513249d
 
+$(document).ready(function(){
 
-var queryURL = "https://api.edamam.com/api/food-database/parser?ingr=brown%20rice&category=fast-foods&app_id=f6a7e516&app_key=d2d2590ec9988c392da648e07513249d";
+$("#submit").on("click", function(){
+console.log("clicked");
+    var food = $("#food").val();
+    var queryURL = "https://api.edamam.com/api/food-database/parser?ingr="+ food + "&category=fast-foods&app_id=f6a7e516&app_key=d2d2590ec9988c392da648e07513249d";
 
 $.ajax({
     url: queryURL,
     method: "GET"
   })
-
   //Response handler
-    .then(function(response) {
+ .then(function(response) {
 
-    //Store the data for image_original_url in imageUrl
-      console.log(response);
-    });
+      
+    for (i = 1; i < 20; i++){
+    var restaurantName = response.hints[i].food.brand;
+
+    console.log(restaurantName);
+     
+    }
+    console.log(response);
+ });
+
+});
+
+})
