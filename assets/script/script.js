4077a5d277307088f6ea053ea97bb7e98f682882
Andrew Lu <awclu@ucdavis.edu>
2019-11-11 19:54:00 -0800

    added comments

--- assets/script/script.js
+++ assets/script/script.js
@@ -26,10 +26,10 @@ var database = firebase.database();
 
 //**************************************************************************
 
-//GET LOCATION**************************************************************
+//Function to get your current location*************************************
 function getLocation() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
+      navigator.geolocation.getCurrentPosition(showPosition); 
     } else { 
       x.innerHTML = "Geolocation is not supported by this browser.";
     }
@@ -51,29 +51,40 @@ function getLocation() {
 
 //DOCU READY FUNCTION******************************************************
 $(document).ready(function(){
-  getLocation();
-
+  //This function will make a prompt asking the user that the browser wants to know your location
+  getLocation(); 
+  
+//on click function for submit
 $("#submit").on("click", function(){
    
     
     console.log("clicked");
-    var food = $("#food").val();
+    
+    //Get input for the food type
+    var food = $("#food").val(); 
+
+    //Set the Edamam queryURL
     var queryURL = "https://api.edamam.com/api/food-database/parser?ingr="+ food + "&category=fast-foods&app_id=f6a7e516&app_key=d2d2590ec9988c392da648e07513249d";
 
-    
+ 
+//Function to call the ajax    
 function ajax(){
-
+    
+  //Ajax Request
     $.ajax({
       url: queryURL,
       method: "GET"
     })
+   
     //Response handler
    .then(function(response) {
       restaurantArr = [];
+      
       for (i = 1; i < 20; i++){
       var restaurantName = response.hints[i].food.brand;
       var itemName = response.hints[i].food.label;
       
+      //Add the new restaurant onto the restaurant array used later in google maps api to output restaurant markers onto the map interface
       restaurantArr.push(restaurantName);
       // console.log("Restaurant" +i + ": " + restaurantName);
       // console.log("Name of Dish " + i + ": " + itemName);
@@ -84,37 +95,33 @@ function ajax(){
    });
 
 }
+  //Function calls
   ajax();
   initMap();
   
 });
-
-
-
 // $("#mapObj").attr("src", "https://maps.googleapis.com/maps/api/js?key=AIzaSyBI7n0cKjaMhAdWPnXVmN8GpuFU5xiDahg&callback=initMap&libraries=places");
-    
-
-
 })
-
-
-
-
 //*************************************************************************
 
 //MAP INITIALIZING*********************************************************
 function initMap(){
+    //Using the information received from the broswer of your current location, set a location parameter for google maps api
     var location = new google.maps.LatLng(lat, lng);
     
     infowindow = new google.maps.InfoWindow();
   
+    //Set it so it is centered on your current location
     map = new google.maps.Map(
-        document.getElementById('map'), {center: location, zoom: 15});
+        document.getElementById('map'), {center: location, zoom: 10});
   
     
    
+  //Loop through all the items on the restaurantArr to put markers
   for (i = 0; i < restaurantArr.length; i++){
-        var request = {
+      
+      //The request specifications for google maps search
+      var request = {
       query: restaurantArr[i],
       fields: ['name', 'geometry'],
       
@@ -122,9 +129,10 @@ function initMap(){
     console.log(restaurantArr[i]);
     
      
-  
+    //Initialize places service from Google Maps API
     var service = new google.maps.places.PlacesService(map);
   
+    //Function to request information using .findPlaceFromQuery
     service.findPlaceFromQuery(request, function(results, status) {
       if (status === google.maps.places.PlacesServiceStatus.OK) {
         for (var i = 0; i < results.length; i++) {
